212e8e32baacd4462a461da845fb9c89af99423a
Alexandru C Ipsalat <alexipsalat@hotmail.com>
2022-06-29 14:26:13 +0100

    Win/ loss condition and modal added

--- src/components/Alexdle.js
+++ src/components/Alexdle.js
@@ -1,27 +1,34 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import useAlexdle from '../hooks/useAlexdle'
 import Grid from './Grid'
 import Keypad from './Keypad'
+import Modal from './Modal'
 
 export default function Alexdle({solution}) {
     const {currentGuess, handleKeyup, guesses, isCorrect, turn, usedKeys} = useAlexdle(solution)
+    const [showModal, setShowModal] = useState(false)
 
     useEffect(() => {
         window.addEventListener('keyup', handleKeyup)
-        return () => window.removeEventListener('keyup', handleKeyup)
-    }, [handleKeyup])
 
-    useEffect(() => {
-        console.log(guesses, turn, isCorrect)
-    }, [guesses, turn, isCorrect])
+        if(isCorrect){
+          setTimeout(() => setShowModal(true), 2000)
+          window.removeEventListener('keyup', handleKeyup)
+        }
+        else if(turn > 5){
+          setTimeout(() => setShowModal(true), 2000)
+          window.removeEventListener('keyup', handleKeyup)
+        }
+
+        return () => window.removeEventListener('keyup', handleKeyup)
+    }, [handleKeyup, isCorrect])
 
   return (
     <div>
         <Grid currentGuess={currentGuess} guesses={guesses} turn={turn}/>
         <br></br>
-        <div>Current Guess - <b>{currentGuess}</b> </div>
-        <div>Solution - <b>{solution}</b> </div>
         <Keypad usedKeys={usedKeys} />
+        {showModal && <Modal isCorrect={isCorrect} turn={turn} solution={solution} /> }
     </div>
   )
 }
